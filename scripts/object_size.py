a4a00cdf2ca1dfd45fd92401e1cdb85e387c83e7
anhhuynh2111 <anhhuynh98@gmail.com>
2017-10-24 20:59:18 +0800

    Update object_size Hieu

--- scripts/object_size.py
+++ scripts/object_size.py
@@ -3,6 +3,7 @@
 # python object_size.py --image images/example_02.png --width 0.955
 # python object_size.py --image images/example_03.png --width 3.5
 
+# import the necessary packages
 from scipy.spatial import distance as dist
 from imutils import perspective
 from imutils import contours
@@ -24,6 +25,7 @@ args = vars(ap.parse_args())
 
 # load the image, convert it to grayscale, and blur it slightly
 image = cv2.imread(args["image"])
+print(image.shape)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
@@ -45,51 +47,86 @@ pixelsPerMetric = None
 
 # loop over the contours individually
 for c in cnts:
+
 	# if the contour is not sufficiently large, ignore it
 	if cv2.contourArea(c) < 100:
 		continue
 
+	# compute the rotated bounding box of the contour
 	orig = image.copy()
 	box = cv2.minAreaRect(c)
 	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
 	box = np.array(box, dtype="int")
-	
-	box = perspective.order_points(box)
-	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
 
-	for (x, y) in box:
-		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
-		
+
+
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+
+
+    # we will identify if the contour is rectangle or circle
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+	if(len(approx) == 4):
+		print("RECTANGLE")
+		# loop over the original points and draw them
+		for (x, y) in box:
+			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+			cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+	else:
+		print("CIRCLE")
+		(x,y),radius = cv2.minEnclosingCircle(c)
+		center = (int(x),int(y))
+		radius = int(radius)
+		cv2.drawContours(orig, [c], -1, (0, 255, 0), 2)
+
+	# unpack the ordered bounding box, then compute thenhe midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
 	(tl, tr, br, bl) = box
 	(tltrX, tltrY) = midpoint(tl, tr)
 	(blbrX, blbrY) = midpoint(bl, br)
 
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
 	(tlblX, tlblY) = midpoint(tl, bl)
 	(trbrX, trbrY) = midpoint(tr, br)
 
+	# draw the midpoints on the image
 	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
 	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
 	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
 	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
+	# draw lines between the midpoints
 	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
 		(255, 0, 255), 2)
 	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
 		(255, 0, 255), 2)
 
+	# compute the Euclidean distance between the midpoints
 	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
 	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
+	# if the pixels per metric has not been initialized, then
+	# compute it as the ratio of pixels to supplied metric
+	# (in this case, inches)
 	if pixelsPerMetric is None:
-		pixelsPerMetric = dB / args["width"]
+		frameWidth = image.shape[1];
+		pixelsPerMetric = frameWidth / args["width"]
 
+	# compute the size of the object
 	dimA = dA / pixelsPerMetric
 	dimB = dB / pixelsPerMetric
 
-	cv2.putText(orig, "{:.1f}in".format(dimA),
+	# draw the object sizes on the image
+	cv2.putText(orig, "{:.1f}in".format(dimB),
 		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
 		0.65, (255, 255, 255), 2)
-	cv2.putText(orig, "{:.1f}in".format(dimB),
+	cv2.putText(orig, "{:.1f}in".format(dimA),
 		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
 		0.65, (255, 255, 255), 2)
 
