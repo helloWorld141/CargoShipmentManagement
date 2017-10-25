ceebd8c1eb3ba16c25cbd95aead487e9f7edf9e6
Nghia Nguyen <dannguyen@paypal.com>
2017-10-25 10:10:16 +0800

    modify django settings to deploy. add gitignore

--- cargo_dimension/cargo_dimension/settings.py
+++ cargo_dimension/cargo_dimension/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
